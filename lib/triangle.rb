38384f7fa4305f007ba13ffb0e07d7a177811271
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-05-25 13:01:17 +0300

    done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
-  # write code here
+  # Custom error class for triangle-related errors
+  class TriangleError < StandardError
+  end
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    validate_triangle
+  end
+
+  def kind
+    if @side1 == @side2 && @side2 == @side3
+      :equilateral
+    elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+
+  def validate_triangle
+    validate_positive_sides
+    validate_triangle_inequality
+  end
+
+  def validate_positive_sides
+    raise TriangleError if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
+  end
+
+  def validate_triangle_inequality
+    raise TriangleError if @side1 + @side2 <= @side3 ||
+                           @side2 + @side3 <= @side1 ||
+                           @side1 + @side3 <= @side2
+  end
 end
+
